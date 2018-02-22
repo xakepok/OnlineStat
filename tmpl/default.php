df2df0288e7e12cf1e591dc2f25f4669e641a82c
xakepok <xakepok@xakepok.com>
2018-02-22 17:35:29 +0300

    Внешний вид

--- tmpl/default.php
+++ tmpl/default.php
@@ -3,8 +3,8 @@ defined('_JEXEC') or die;
 $domain = $params->get('domain', 'https://www.joomla.org');
 ?>
 <div style="font-size: 0.7em;"><?php echo JText::_('MOD_ONLINESTAT_WELCOME'); ?></div>
-<div>
-    <table>
+<div style="width: 100%;">
+    <table style="width: 100%;">
         <thead>
             <tr>
                 <th><?php echo JText::_('MOD_ONLINESTAT_HEAD_DIRECTION'); ?></th>
@@ -15,11 +15,20 @@ $domain = $params->get('domain', 'https://www.joomla.org');
         <tbody>
             <?php foreach ($stat as $item): ?>
             <tr>
-                <td><?php echo $item->dir;?></td>
-                <td><?php echo $item->day;?> %</td>
-                <td><?php echo $item->week;?> %</td>
+                <td style="<?php if ($item->dir == 'Суммарно') echo "font-weight: bold;"; ?>">
+                    <?php echo $item->dir;?>
+                </td>
+                <td style="text-align: center; <?php if ($item->dir == 'Суммарно') echo "font-weight: bold;"; ?>">
+                    <?php echo $item->day;?> <span style="font-size: 0.7em;">%</span>
+                </td>
+                <td style="text-align: center; <?php if ($item->dir == 'Суммарно') echo "font-weight: bold;"; ?>">
+                    <?php echo $item->week;?> <span style="font-size: 0.7em;">%</span>
+                </td>
             </tr>
             <?php endforeach;?>
         </tbody>
     </table>
+</div>
+<div style="text-align: right;">
+    <a style="font-size: 0.8em; font-style: italic;" class="jutooltip" title="<?php echo JText::_('MOD_ONLINESTAT_HELP_TEXT'); ?>"><?php echo JText::_('MOD_ONLINESTAT_HELP'); ?>?</a>
 </div>
